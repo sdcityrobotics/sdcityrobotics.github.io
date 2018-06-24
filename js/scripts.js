70d25cb8ecb2f87273d858909515f68e3588858a
Emma Brand <emmalanebrand@gmail.com>
2018-06-24 09:17:57 -0700

    New site design and test content
    Former-commit-id: 19047f43437b7f14266a86272ec19f63222f29c7 [formerly 4a398b8415a8a9f528f21c8079ed5eca2365da0e]
    Former-commit-id: 779f6ec314a6a749fccb3b8248ed011bb18cc28d

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+$(function() {    
+    // scrolltop and header animate --------------------------//
+    var lastScrollTop = 0;
+    var delta = 50;
+    
+    if($(this).scrollTop() > 0) {
+          $('header').addClass('nav-down');
+    }
+  
+    $('body').scroll(function(){
+      var st = $(this).scrollTop();
+  
+      if(Math.abs(lastScrollTop - st) <= delta) {
+        return;
+      }
+  
+      if (st > lastScrollTop){
+        $('header').removeClass('nav-down').addClass('nav-up');
+        $('#scrolltop').removeClass('up');
+      } else {
+        if(st + $(window).height() < $(document).height() && st >= delta) {
+          $('header').removeClass('nav-up').addClass('nav-down');
+          $('#scrolltop').addClass('up');
+        } else {
+          $('header').removeClass('nav-up').removeClass('nav-down');
+          $('#scrolltop').removeClass('up');
+        }
+      }
+      lastScrollTop = st;
+
+      if($(window).width() >= 768){
+        if($('.parallax.bottom').length && $(window).scrollTop() >= $('.parallax.bottom').offset().top - 200) {
+          $('.parallax.bottom').css('padding-top','50vh');
+        }
+  
+        if($('.parallax.right').length && $(window).scrollTop() >= $('.parallax.right').offset().top - 200) {
+          $('.parallax.right .content').css('margin-left','60vw');
+          $('.parallax.right .content').css('right','0');
+        }
+      }
+
+      lazyLoad();
+    });
+  
+  
+    $('#scrolltop').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1000);
+    });
+
+    $('body').on('click','a.next',function(){
+      var $hash = $($(this).prop('hash'));
+      var hashTop = -($hash.parent().offset().top) + $(window).innerHeight();
+      $('html, body').animate({
+        scrollTop: hashTop
+      }, 1000);
+    });
+
+    lazyLoad();
+
+  });
+
+  function lazyLoad(){
+    $('.lazy').each(function(){
+      if($(this).attr('data-src') != '' && $(window).scrollTop() >= $(this).offset().top - $(window).innerHeight()) {
+        if($(this).is('img')){
+          $(this).attr('src',$(this).attr('data-src')).css('opacity',100);
+        } else if($(this).is('section') || $(this).is('div')){
+          $(this).css('background-image','url('+$(this).attr('data-src')+')').css('opacity',100);
+        }
+        $(this).attr('data-src','');
+      }
+    });
+  }
